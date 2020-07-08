5a2060fd683fecf5ed13ac3fb8fe6df55a5449ac
David Dickson <davidkdickson@gmail.com>
2020-07-07 21:02:48 -0700

    step out now working

--- debugger.py
+++ debugger.py
@@ -4,14 +4,19 @@ from multiprocessing import Process, Queue
 
 
 def inner():
-    return 1
+    x = 1 + 2
+    y = 2 + x
+    return y
 
 
 def sample(arg_a, arg_b):
     addition = arg_a + arg_b
     multiply = addition * arg_a * arg_b
     inner()
+    y = 100 + 200
     print('Sample: ' + str(multiply))
+    y = 200
+    y = 300
 
 
 def print_source(frame):
@@ -30,14 +35,12 @@ def print_source(frame):
         if idx in [index - 1, index + 1] and index > 0:
             print(source_line.rstrip())
 
+def get_command():
+    print('(pydbg)', end=" ", flush=True)
+    return commands.get()
 
 def trace_calls(frame, event, arg):
-    print('(pydbg)', end=" ", flush=True)
-    cmd = commands.get()
-    if cmd == 's':
-        return trace_lines
-    if cmd == 'n':
-        return
+    print(f'trace_calls: {event}')
     return trace_lines
 
 
@@ -46,18 +49,24 @@ commands = Queue()
 
 
 def trace_lines(frame, event, arg):
+    if event != 'line':
+        print(f'trace_lines: {event}')
+        return
+
     print_source(frame)
-    print('(pydbg)', end=" ", flush=True)
-    cmd = commands.get()
+
+    cmd = get_command()
 
     if cmd == 's':
         return trace_lines
-
     if cmd == 'n':
+        print('next')
         return trace_calls
+    if cmd == 'f':
+        del frame.f_trace
+        return None
 
-    if cmd == 'q':
-        raise 'stop execution'
+    raise 'unknown command'
 
 
 def debug():
@@ -72,9 +81,8 @@ for line in sys.stdin:
     command = line.split()
     if command[0] == 'q':
         print('qutting debugger')
-        commands.put('q')
+        p.terminate()
         break
-
     if command[0] == 'b':
         file, line = command[1].split(':')
         breakpoints[f'{file}:{line}'] = True
@@ -83,3 +91,7 @@ for line in sys.stdin:
         commands.put('c')
     if command[0] == 's':
         commands.put('s')
+    if command[0] == 'n':
+        commands.put('n')
+    if command[0] == 'f':
+        commands.put('f')
