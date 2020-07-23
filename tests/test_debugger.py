a5ff8f6633773ffa4a4f8dc2d978ea9cb1255b16
David Dickson <davidkdickson@gmail.com>
2020-07-22 19:14:03 -0700

    added test for getting the location

--- tests/test_debugger.py
+++ tests/test_debugger.py
@@ -1,7 +1,13 @@
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 from pydbg.debugger import dbg
 
 @patch('builtins.print')
 def test_prompt(mocked_print):
     dbg.prompt()
     assert mocked_print.mock_calls == [call('(pydbg)', end=' ', flush=True)]
+
+def test_location():
+    frame = MagicMock()
+    frame.f_code.co_filename = 'file'
+    frame.f_lineno = 21
+    assert dbg.get_location(frame) == 'file:21'
