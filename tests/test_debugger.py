453761e2ff75b631c16d41fe81f0d7ab35910f02
David Dickson <davidkdickson@gmail.com>
2020-08-02 11:32:52 -0700

    begin adding test for printing source

--- tests/test_debugger.py
+++ tests/test_debugger.py
@@ -11,3 +11,14 @@ def test_location():
     frame.f_code.co_filename = 'file'
     frame.f_lineno = 21
     assert dbg.get_location(frame) == 'file:21'
+
+@patch('builtins.print')
+@patch('inspect')
+def test_print_source(mocked_print, mocked_inspect):
+    inspect.getsourcelines
+    frame = MagicMock()
+    frame.code = "hello bob\nhello sam\nhello tim"
+    frame.code.co_name = 'function'
+    frame.f_lineno = 12
+    frame.co_filename = 'file'
+
