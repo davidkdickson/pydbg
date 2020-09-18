0b105281ee5fe3df90709eff7d303276f681caf8
David Dickson <davidkdickson@gmail.com>
2020-09-17 22:06:56 -0700

    adding type checking

--- examples/blog.py
+++ examples/blog.py
@@ -1,25 +1,26 @@
 import sys
 import inspect
+from types import FrameType
 
 class Pydbg:
 
-    def print_source(self, frame, event, arg):
+    def print_source(self, frame: FrameType):
         (fil, line, func, code, idx) = inspect.getframeinfo(frame)
         line = f'{fil}:{line}:{func}\n--> {code[idx]}'
         print(line)
+
 
     def get_command(self):
         return 's'
 
 
-    def trace_calls(self, frame, event, arg):
-        self.print_source(frame, event, arg)
+    def trace_calls(self, frame: FrameType, _event, _arg):
+        self.print_source(frame)
         self.cmd = self.get_command()
 
         if self.cmd == 's':
             return self.trace_calls
 
-        self.print_source(frame, event, arg)
 
         raise 'Unknown command'
 
@@ -27,16 +28,13 @@ class Pydbg:
     def break_point(self):
         sys.settrace(self.trace_calls)
 
-
-
 if __name__ == "__main__":
     pydbg = Pydbg()
 
-    def fibonacci(n):
-        if n in [0, 1]:
-            return n
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
+    def fibonacci(num) -> int:
+        if num in [0, 1]:
+            return num
+        return fibonacci(num - 1) + fibonacci(num - 2)
 
     pydbg.break_point()
     fibonacci(2)
